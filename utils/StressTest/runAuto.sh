3c94bc03465f76663f720a1f34e5a7e3a75a4d82
getmansky <getmansky@95891348-1de2-4420-a55f-93115de88a4c>
2016-12-30 10:45:30 +0000

    ~ add run scripts and cmd params

---
+++ utils/StressTest/runAuto.sh
@@ -0,0 +1,8 @@
+_Debug/bin/test 10 10 0
+_Debug/bin/test 20 10 0
+_Debug/bin/test 30 10 0
+_Debug/bin/test 40 10 0
+_Debug/bin/test 50 10 0
+
+
+
---
+++ utils/StressTest/runFMA.sh
@@ -0,0 +1,8 @@
+_Debug/bin/test 10 10 2
+_Debug/bin/test 20 10 2
+_Debug/bin/test 30 10 2
+_Debug/bin/test 40 10 2
+_Debug/bin/test 50 10 2
+
+
+
